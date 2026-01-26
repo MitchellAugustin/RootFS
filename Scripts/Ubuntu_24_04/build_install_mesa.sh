528a8c1095a5e2e9d288a224b9e3c71729755960
Mitchell Augustin <mitchell.augustin@canonical.com>
2026-01-26 17:38:45 -0600

    Add libraries required for RPi and Snapdragon

--- Scripts/Ubuntu_24_04/build_install_mesa.sh
+++ Scripts/Ubuntu_24_04/build_install_mesa.sh
@@ -13,6 +13,7 @@ apt-get install -y software-properties-common
 # Just use kisk-ppa
 # Debian/Ubuntu llvm development packages have been broken for multiple releases.
 add-apt-repository -y ppa:kisak/kisak-mesa
+add-apt-repository -y ppa:mitchellaugustin/steam-snap-enablement
 
 apt-get update
 apt-get upgrade -y
@@ -21,7 +22,7 @@ apt-get install -y libgl1 libgl1:i386 mesa-va-drivers mesa-vdpau-drivers mesa-vu
 
 apt-get upgrade -y
 
-apt-mark hold \
+apt-get install -y \
   libegl-mesa0 libegl-mesa0:i386 libgbm1 libgbm1:i386 libgl1-mesa-dri \
   libgl1-mesa-dri:i386 libglx-mesa0 libglx-mesa0:i386 libllvm20 libllvm20:i386 \
   mesa-libgallium mesa-libgallium:i386 mesa-va-drivers mesa-va-drivers:i386 \
