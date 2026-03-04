a8fcb69251e1356f2b641ce0efe52c1989d245da
Mitchell Augustin <mitchell.augustin@canonical.com>
2026-03-04 09:56:56 -0600

    Fix additional platform drivers in rootfs build

--- Scripts/Ubuntu_24_04/build_install_mesa.sh
+++ Scripts/Ubuntu_24_04/build_install_mesa.sh
@@ -13,18 +13,16 @@ apt-get install -y software-properties-common
 # Just use kisk-ppa
 # Debian/Ubuntu llvm development packages have been broken for multiple releases.
 add-apt-repository -y ppa:kisak/kisak-mesa
-add-apt-repository -y ppa:mitchellaugustin/steam-snap-enablement
+add-apt-repository -y ppa:tobhe/mesa-test
 
 apt-get update
 apt-get upgrade -y
-apt-get install -y libgl1 libgl1:i386 mesa-va-drivers mesa-vdpau-drivers mesa-vulkan-drivers libglx-mesa0 libgl1-mesa-dri \
+apt-get install -y libgl1 libgl1:i386 mesa-vulkan-drivers libglx-mesa0 libgl1-mesa-dri \
     libegl-mesa0
 
 apt-get upgrade -y
-
 apt-get install -y \
   libegl-mesa0 libegl-mesa0:i386 libgbm1 libgbm1:i386 libgl1-mesa-dri \
   libgl1-mesa-dri:i386 libglx-mesa0 libglx-mesa0:i386 libllvm20 libllvm20:i386 \
-  mesa-libgallium mesa-libgallium:i386 mesa-va-drivers mesa-va-drivers:i386 \
-  mesa-vdpau-drivers mesa-vdpau-drivers:i386 mesa-vulkan-drivers \
+  mesa-libgallium mesa-libgallium:i386 mesa-vulkan-drivers \
   mesa-vulkan-drivers:i386
